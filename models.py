6018ad266748ddb1f6aecbc9c9ec88817b5b23c8
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-07-17 01:11:54 +0100

    Task creator

---
+++ models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from bson import ObjectId
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+
+class TaskBase(BaseModel):
+    title: str
+    completed: bool = False
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    completed: Optional[bool] = None
+
+class TaskResponse(TaskBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+        json_encoders = {
+            ObjectId: str
+        }
